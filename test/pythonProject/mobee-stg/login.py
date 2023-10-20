45f84fb7fd6183715f60f90e227bfc04146db725
Absari Hanum Ramadhani H <ahrh2413@ymail.com>
2023-10-21 03:07:42 +0700

    mobee-1273-loginsignup-screen

--- test/pythonProject/mobee-stg/login.py
+++ test/pythonProject/mobee-stg/login.py
@@ -21,11 +21,15 @@ cap:Dict[str, Any]={
     "appium:autoGrantPermissions": "true",
     "appium:connectHardwareKeyboard": "true",
 }
-# driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", caps)
+
 driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", options=AppiumOptions().load_capabilities(cap))
+
+# Launch the Mobee App
 time.sleep(10)
 el1 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Financial Freedom\nStarts Here\nLogin/Sign Up\nPhone Number")
 el1.click()
+
+# Input phone number
 el2 = driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.EditText")
 el2.click()
 time.sleep(5)
@@ -38,28 +42,32 @@ TouchAction(driver).tap(None, 147, 1610, 1).perform()
 TouchAction(driver).tap(None, 408, 2025, 1).perform()
 TouchAction(driver).tap(None, 147, 1610, 1).perform()
 time.sleep(5)
+# disappear keyboard on screen
 driver.hideKeyboard()
-# el3 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Login/Sign Up\nPhone Number")
-# el3.click()
+
 time.sleep(3)
 el4 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Continue")
 el4.click()
+
+# Input password
 time.sleep(5)
 el5 = driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.EditText")
 el5.click()
 el5.send_keys("Testing123")
 driver.hideKeyboard()
-# el6 = driver.find_element(by=AppiumBy.XPATH, value="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView")
-# el6.click()
 time.sleep(3)
 el7 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Log In")
 el7.click()
+
+# OTP Email
 time.sleep(3)
 el8 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Verify")
 el8.click()
+ 
+# Choose one to un/command for using biometrics
 el9 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Not Now")
 el9.click()
 # el9 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Enable Biometrics")
 # el9.click()
 
-# driver.quit()
+driver.quit()
